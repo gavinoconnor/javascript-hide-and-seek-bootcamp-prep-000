cbe733f5244b69d5ad0804acb62e1ea3ecd2fcc1
Gavin O'Connor <gedmund@gmail.com>
2019-03-26 19:45:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function getFirstSelector(selector) {
 //   (`#` is used for IDs in selectors — but you knew that because you [read the docs][docs],
 //   right? :) ). (Note that in `index.html` `#nested` and `.target` just
 //   _happen_ to be `div`s. This method should work with arbitrary elements.)
-//
 function nestedTarget() {
   var container = document.querySelector("#nested")
   var matches = container.querySelector(".target")
@@ -17,8 +16,16 @@ function nestedTarget() {
 //
 // - Define a function `increaseRankBy(n)` that increases the ranks in all of the
 //   `.ranked-list`s by `n`. (You might need to make use of [`parseInt()`][parseint]
-//
-//
+function increaseRankBy(n) {
+  const rankedLists = document
+  .getElementById('app')
+  .querySelectorAll('ul.ranked-list')
+  
+  for (let i = 0; i < rankedLists.length; i++) {
+    rankedLists[i].innerHTML = (i + n)
+  }
+}
+
 // - Define a function `deepestChild()` that pulls out the most deeply nested child
 //   element from `div#grand-node`. (Remember, you can iterate over elements and call
 //   `querySelector()` and `querySelectorAll()` on them. This is challenging to
