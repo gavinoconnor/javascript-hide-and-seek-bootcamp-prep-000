2ca149ae88fad5dd6e568bebd9b78fa190f83f03
Gavin O'Connor <gedmund@gmail.com>
2019-03-26 20:21:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ function getFirstSelector(selector) {
 //   right? :) ). (Note that in `index.html` `#nested` and `.target` just
 //   _happen_ to be `div`s. This method should work with arbitrary elements.)
 function nestedTarget() {
-  var container = document.querySelector('#nested')
-  var matches = container.querySelector('.target')
-  return matches
+  return document.querySelector('#nested .target')
 }
 // - Define a function `increaseRankBy(n)` that increases the ranks in all of the
 //   `.ranked-list`s by `n`. (You might need to make use of [`parseInt()`][parseint]
